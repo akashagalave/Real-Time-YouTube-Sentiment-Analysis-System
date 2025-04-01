9d2f5e980223e80c407ba54b9f670d8b1430d217
akashagalave <akashagalaveaaa1@gmail.com>
2025-04-01 18:24:15 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This End To End Project Will Help For Analysis Of YouTube Comments.',
+    author='akash agalave',
+    license='',
+)
