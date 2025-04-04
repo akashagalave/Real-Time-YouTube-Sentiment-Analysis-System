1d93af009a3dc05b9358140eb4b78f4deb3c1054
akashagalave <akashagalaveaaa1@gmail.com>
2025-04-04 13:30:32 +0530

    Chnages in Flask_Test Code

--- scripts/test_flask_api.py
+++ scripts/test_flask_api.py
@@ -6,7 +6,7 @@ BASE_URL = "http://localhost:5000"  # Replace with your deployed URL if needed
 
 def test_predict_endpoint():
     data = {
-        "comments": ["This is a great product!", "Not worth the money.", "It's okay."]
+        "comments": ["This is a good product!", "Not worth the money.", "It's okay."]
     }
     response = requests.post(f"{BASE_URL}/predict", json=data)
     assert response.status_code == 200
